343fdbdc1ffa9bf70937c3b84acdf66badbd099a
mbugua <mbuguafaimba@gmail.com>
2023-02-06 21:44:40 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,54 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
+
+puts admin_login("ADMIN",12345)
 
 def hows_the_weather(temperature)
-  # your code here
+  if (temperature < 40)
+    return "It's brisk out there!"
+  elsif (temperature >= 40 && temperature <= 65)
+    return "It's a little chilly out there!"
+  elsif (temperature > 85)
+    return "It's too dang hot out there!"
+  else
+    return  "It's perfect out there!"
+  end
 end
+puts hows_the_weather(76)
+
 
 def fizzbuzz(num)
-  # your code here
+  if (num % 3 === 0 && num % 5 === 0)
+    return "FizzBuzz"
+  elsif (num % 3 === 0 )
+    return "Fizz"
+  elsif (num % 5 === 0)
+    return "Buzz"
+  else
+    return num
+  end
 end
+puts fizzbuzz(5)
 
 def calculator(operation, num1, num2)
-  # your code here
+  case (operation)
+    when "+"
+       num1 + num2
+    when "-"
+       num1 - num2
+    when "*"
+       num1 * num2
+    when "/"
+       num1 / num2
+    else
+      puts "Invalid operation!"
+      nil
+  end
 end
+puts calculator("+",3,4)
 
